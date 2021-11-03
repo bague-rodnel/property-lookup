11ae09d7f891298ee5b84d3a5e16f13b3a3a0f97
Rodnel Bague <bague.rodnel@gmail.com>
2021-11-04 01:28:15 +0800

    working connecting between client and backend(api)

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  console.log(apiResponse);
+
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => {
+        setApiResponse(res);
+      });
+  };
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="App__intro">{apiResponse}</p>
+    </div>
+  );
+}
+
+export default App;
