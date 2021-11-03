138c1ca8d663d5787f1edaaf064d86a10e1214ae
Rodnel Bague <bague.rodnel@gmail.com>
2021-11-04 01:52:52 +0800

    successfully exposed graphql endpoint in the api still learning how to consume on client

--- api/app.js
+++ api/app.js
@@ -5,9 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
+var graphQLRoute = require("./routes/graphQL");
 
 var app = express();
 
@@ -22,9 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
+// app.use("/", indexRouter);
+app.use("/graphql", graphQLRoute);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
